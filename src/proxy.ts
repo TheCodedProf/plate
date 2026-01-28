87df0564c499afacedfe3b09c536698ccc75bea4
Samuel Shuert <me@thecoded.prof>
2026-01-28 21:56:54 +0000

    feat: auth api routes

---
+++ src/proxy.ts
@@ -0,0 +1,34 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+
+export async function proxy(request: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const pathSegments = request.nextUrl.pathname.split("/").filter(Boolean);
+
+  if (!session && !pathSegments.includes("auth")) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
+  if (pathSegments[0] === "dashboard" && pathSegments.length === 1) {
+    return NextResponse.redirect(new URL("/dashboard/main", request.url));
+  }
+
+  if (
+    pathSegments[0] === "auth" &&
+    (pathSegments.length === 1
+      ? true
+      : ["signin", "signup"].includes(pathSegments[1])) &&
+    session
+  ) {
+    return NextResponse.redirect(new URL("/dashboard/main", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:id*", "/auth/:action"],
+};
