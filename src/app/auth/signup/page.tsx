7a01e68db324f0d6422b2f1ce83b95f7c4f00381
Samuel Shuert <me@thecoded.prof>
2026-01-28 21:56:54 +0000

    feat: auth api routes

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { type FormEvent, useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+export default function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const router = useRouter();
+
+  const handleSignUp = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      await authClient.signUp.email({
+        name,
+        email,
+        password,
+      });
+      router.push("/dashboard");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-2 h-screen bg-ctp-base text-ctp-text">
+      <div />
+      <div className="flex justify-center items-center">
+        <form
+          onSubmit={handleSignUp}
+          className="flex flex-col gap-12 mx-40 bg-ctp-surface0 rounded-lg px-8 py-12"
+        >
+          <p className="text-2xl font-bold text-center -mb-8">Sign Up</p>
+          <label className="flex flex-col gap-2 text-xs text-ctp-subtext0">
+            Name:
+            <input
+              type="text"
+              value={name}
+              required
+              onChange={(e) => setName(e.target.value)}
+              className="border border-ctp-overlay2 rounded-md px-2 py-1 text-lg"
+            />
+          </label>
+          <label className="flex flex-col gap-2 text-xs text-ctp-subtext0">
+            Email:
+            <input
+              required
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border border-ctp-overlay2 rounded-md px-2 py-1 text-lg"
+            />
+          </label>
+          <label className="flex flex-col gap-2 text-xs text-ctp-subtext0">
+            Password:
+            <input
+              required
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-ctp-overlay2 rounded-md px-2 py-1 text-lg"
+            />
+          </label>
+          <button
+            className="rounded-md px-2 py-1 text-lg cursor-pointer bg-ctp-green-600 font-bold text-ctp-surface0"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
