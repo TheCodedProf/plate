fd85b6f60644b6acc2e86a955e0274fab59e11d0
Samuel Shuert <me@thecoded.prof>
2026-01-30 16:00:00 +0000

    fix: remove old signout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 "use client";
-import { CalendarWidget } from "@/components/Calendar";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-ctp-mantle font-sans">
-      <main className="flex min-h-screen w-full justify-between p-12 bg-ctp-base">
-        {session?.session.token ? (
-          <button onClick={async () => await authClient.signOut()}>
-            Sign out
-          </button>
-        ) : (
-          <button onClick={() => router.push("/auth/signin")}>Sign in</button>
-        )}
-      </main>
+      <main className="flex min-h-screen w-full justify-between p-12 bg-ctp-base"></main>
     </div>
   );
 }
